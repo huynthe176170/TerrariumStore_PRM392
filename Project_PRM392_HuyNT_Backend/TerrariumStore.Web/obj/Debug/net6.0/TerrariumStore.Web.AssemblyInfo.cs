e66d6b9ed7bf99a3a434d71a3cb694c83ffb13ca
huynthe176170 <157361985+huynthe176170@users.noreply.github.com>
2025-03-25 14:03:37 +0700

    update code

--- Project_PRM392_HuyNT_Backend/TerrariumStore.Web/obj/Debug/net6.0/TerrariumStore.Web.AssemblyInfo.cs
+++ Project_PRM392_HuyNT_Backend/TerrariumStore.Web/obj/Debug/net6.0/TerrariumStore.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TerrariumStore.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fab2895130f85478b2f9ded9cf54b0b2de2bd3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0598c9fce073a726bacc29e400f509fa28b2cb95")]
 [assembly: System.Reflection.AssemblyProductAttribute("TerrariumStore.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TerrariumStore.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
